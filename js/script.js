01a06bdb57a5bea7bdd2b92da4c8320bf8891521
Ignacio Gabriel Brasesco <brasescoignaciogabriel@gmail.com>
2024-04-12 21:19:46 -0300

    ejercicio 1.0 resuelto

---
+++ js/script.js
@@ -0,0 +1,11 @@
+let usuario = prompt("Escriba su nombre");
+let saldoDeCuenta = 1000;
+let montoARetirar = prompt("¿Cuanto desea retirar?");
+
+if(saldoDeCuenta > montoARetirar){
+    saldoDeCuenta - montoARetirar
+    alert("retiro exitoso")
+} else{
+    alert("Saldo insuficiente");
+};
+
